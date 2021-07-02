d222424326ed00bc1609de4f6098258408cb4440
abbasshaik <77068394+abbasshaik@users.noreply.github.com>
2021-07-02 23:52:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const file = require('fs');
+const a =file.readFileSync('C:\\Users\\Shaik Abbas\\Desktop\\ftpCred.txt');
+
+console.log(a.toString());
+console.log("HogayaChekc");
+
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+console.log(port);
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Hi ra puka </h1> <p> Inka nerchukovalsindi chala undi dengey</p>');
+});
+
+server.listen(port, ()=>{
+    console.log("Server is listening to port : "+port);
+});
+
+var text ="aValEl "
+
---
+++ second.js
@@ -0,0 +1,3 @@
+const second = "abbas";
+
+exports.second;
